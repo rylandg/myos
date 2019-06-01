2eee960d511aee457c9141de8e424d074f0c8378
Ryland Goldstein <ryland@binaris.com>
2019-06-01 10:37:20 -0700

    switch to shell CLI, restructure directories

--- myos.sh
+++ myos.sh
@@ -1,96 +1,67 @@
 #!/bin/sh -e -u
 
+MYOS_USER="ubuntu"
+
+initHelp="init <envName> [options]"
+createHelp="create <envName> [options]"
+connectHelp="connect <envName> [options]"
+removeHelp="remove <envName> [options]"
+
 die () {
     echo >&2 "$@"
     exit 1
 }
 
-login () {
-    eval $(aws ecr get-login --no-include-email)
+unknownCommand () {
+  echo "$1" not recognized!
+  exit 1
 }
 
 enforceArgs () {
   if [ "$1" -lt $2 ]; then
-    echo "Minimum of $2 arguments required for create/attach"
+    echo "Minimum of $2 arguments required for $3"
     exit 1
   fi
 }
 
-getAccountId() {
-  accountQuoted=$(aws sts get-caller-identity --output=json | jq '.Account')
-  temp="${accountQuoted%\"}"
-  temp="${temp#\"}"
-  echo "$temp"
-}
+enforceArgs $# 1 "MyOS"
 
-if [ "$#" -lt 1 ]; then
-    die "Minimum of 1 arguments required for MyOS"
+command=$1
+# cross-platform dir resolution
+myosPath="$( cd "$(dirname  "${BASH_SOURCE[0]}")" ; pwd -P )"
+# follows symlink
+scriptName="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
+scriptPath="$myosPath/$scriptName"
+
+if [ $command = "init" ]; then
+  enforceArgs $# 2 $initHelp
+  mkdir $2
+  cp -r $myosPath/templates/. $2/
+elif [ $command = "build" ]; then
+  tag=${3:-latest}
+  docker build -t myos:$tag .
+elif [ $command = "create" ]; then
+  enforceArgs $# 2 $createHelp
+  args="COMPOSE_PROJECT_NAME=$2"
+  if [ "$#" -ge 3 ]; then
+    args="$args NAME=$3"
+  fi
+  if [ "$#" -ge 4 ]; then
+    args="$args TAG=$4"
+  fi
+  export $args && docker-compose up -d
+elif [ $command = "remove" ]; then
+  enforceArgs $# 2 $removeHelp
+  args="COMPOSE_PROJECT_NAME=$2"
+  export $args && docker-compose down
+elif [ $command = "connect" ]; then
+  enforceArgs $# 2 $connectHelp
+  socket=$(docker port "$2_myos_1" 22)
+  port="$(cut -d':' -f2 <<<$socket)"
+  shift 2
+  sshArgs="$@ -Y -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -p $port"
+  ssh $sshArgs $MYOS_USER@localhost
+  exit 0
+else
+  unknownCommand $command
 fi
-
-COMMAND=$1
-USER_DEFAULT=${DEFAULT_USER:-ubuntu}
-
-if [ $COMMAND = "login" ]; then
-  login
-elif [ $COMMAND = "build" ]; then
-  if [ "$#" -lt 2 ]; then
-      docker build -t myos:latest .
-  else
-      docker build --build-arg DEFAULT_USER=$2 -t myos:latest .
-  fi
-  exit 0
-elif [ $COMMAND = "connect" ]; then
-  SOCKET=$(docker-compose port myos 22)
-  PORT="$(cut -d':' -f2 <<<$SOCKET)"
-  shift 1
-  SSH_PARAMS="$@ -Y -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -p $PORT"
-  ssh $SSH_PARAMS $USER_DEFAULT@localhost
-  exit 0
-elif [ $COMMAND = "create" ]; then
-  enforceArgs $# 2
-  if [ "$#" -eq 3 ]; then
-    NAME=$2 TAG=$3 docker-compose up -d
-  else
-    NAME=$2 docker-compose up -d
-  fi
-  exit 0
-elif [ $COMMAND = "restart" ]; then
-  enforceArgs $# 2
-  if [ "$#" -eq 3 ]; then
-    docker stop $2 && docker rm $_
-    NAME=$2 TAG=$3 docker-compose up -d
-  else
-    docker stop $2 && docker rm $_
-    NAME=$2 TAG=latest docker-compose up -d
-  fi
-
-  exit 0
-elif [ $COMMAND = "remove" ]; then
-  enforceArgs $# 2
-  docker stop $2 && docker rm $_
-  exit 0
-elif [ $COMMAND = "push" ]; then
-  login
-  enforceArgs $# 2
-  TAG=$2
-  docker tag myos:$TAG $ACCOUNT_ID.dkr.ecr.us-west-1.amazonaws.com/myos:$TAG
-  docker push $ACCOUNT_ID.dkr.ecr.us-west-1.amazonaws.com/myos:$TAG
-  exit 0
-elif [ $COMMAND = "pull" ]; then
-  login
-  enforceArgs $# 2
-  TAG=$2
-  ACCOUNT_ID=$(getAccountId)
-  docker pull $ACCOUNT_ID.dkr.ecr.us-west-1.amazonaws.com/myos:$TAG
-  docker tag $ACCOUNT_ID.dkr.ecr.us-west-1.amazonaws.com/myos:$TAG myos:$TAG
-  exit 0
-elif [ $COMMAND = "commit" ]; then
-  enforceArgs $# 3
-  NAME=$2
-  TAG=$3
-  docker commit $NAME myos:$TAG
-  exit 0
-fi
-
-echo "$COMMAND" not recognized!
-exit 1
